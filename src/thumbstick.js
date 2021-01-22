72d6b4e73d9ea9c8c7e14a577fc82329d9b2c06d
PutterChez <44222382+PutterChez@users.noreply.github.com>
2021-01-22 18:33:33 +0700

    fix rotation

--- src/thumbstick.js
+++ src/thumbstick.js
@@ -7,9 +7,10 @@ AFRAME.registerComponent('thumbstick-rotate',{
     rotate: function (evt) {
         var camera = document.getElementById("cameraRig");
         var head = document.getElementById("head");
+        var newRotation = '0 ' + camera.getAttribute("rotation") -90 + ' 0';
 
         head.setAttribute('look-controls', {enabled: false})
-        camera.setAttribute('rotation', '0 -90 0');
+        camera.setAttribute('rotation', newRotation);
         head.setAttribute('look-controls', {enabled: true})
         // if (evt.detail.x < -0.95) { camera.setAttribute('rotation', '0 -90 0'); }
         // if (evt.detail.x > 0.95) { camera.setAttribute('rotation', '0 90 0'); }
