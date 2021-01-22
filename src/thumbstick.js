023543f51ce18acba49d47df25c100dbbfa78340
PutterChez <44222382+PutterChez@users.noreply.github.com>
2021-01-22 15:31:03 +0700

    Update thumbstick.js

--- src/thumbstick.js
+++ src/thumbstick.js
@@ -2,10 +2,12 @@ const AFRAME = window.AFRAME;
 
 AFRAME.registerComponent('thumbstick-rotate',{
     init: function () {
-      this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+        this.el.addEventListener('thumbstickmoved', this.logThumbstick);
     },
     logThumbstick: function (evt) {
-      if (evt.detail.x < -0.95) { this.el.components['look-controls'].yawObject.rotation.y -= 1; }
-      if (evt.detail.x > 0.95) { this.el.components['look-controls'].yawObject.rotation.y += 1 }
+        var head = document.getElementById('head');
+
+        if (evt.detail.x < -0.95) { head['look-controls'].yawObject.rotation.y -= 1; }
+        if (evt.detail.x > 0.95) { head['look-controls'].yawObject.rotation.y += 1 }
     }
   });
