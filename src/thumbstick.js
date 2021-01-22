0c2827bc691b5a87f4a5c0d55f840eecec22073e
PutterChez <44222382+PutterChez@users.noreply.github.com>
2021-01-22 17:07:03 +0700

    Update thumbstick.js

--- src/thumbstick.js
+++ src/thumbstick.js
@@ -2,12 +2,12 @@ const AFRAME = window.AFRAME;
 
 AFRAME.registerComponent('thumbstick-rotate',{
     init: function () {
-        this.el.addEventListener('thumbstickmoved', this.rotate);
+        this.el.addEventListener('gripdown', this.rotate);
     },
     rotate: function (evt) {
         var camera = document.getElementById("cameraRig");
-
-        if (evt.detail.x < -0.95) { camera.setAttribute('rotation', '0 -90 0'); }
-        if (evt.detail.x > 0.95) { camera.setAttribute('rotation', '0 90 0'); }
+        camera.setAttribute('rotation', '0 -90 0');
+        // if (evt.detail.x < -0.95) { camera.setAttribute('rotation', '0 -90 0'); }
+        // if (evt.detail.x > 0.95) { camera.setAttribute('rotation', '0 90 0'); }
       }
   });
