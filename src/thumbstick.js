30f6bab7b83cbd69258572c3afe669a45b750ae2
PutterChez <44222382+PutterChez@users.noreply.github.com>
2021-01-29 16:11:20 +0700

    Update thumbstick.js

--- src/thumbstick.js
+++ src/thumbstick.js
@@ -12,12 +12,17 @@ AFRAME.registerComponent('thumbstick-rotate',{
         var prevZ = camera.getAttribute("rotation").z;
         var newY;
 
-        if (evt.detail.x < -0.95) { newY = camera.getAttribute("rotation").y + 10; }
-        if (evt.detail.x > 0.95) { newY = camera.getAttribute("rotation").y - 10; }
+        if (evt.detail.x < -0.95) { 
+            newY = camera.getAttribute("rotation").y + 10; 
+            camera.setAttribute('rotation', {x: prevX, y: newY, z: prevZ });
+        }
+        if (evt.detail.x > 0.95) { 
+            newY = camera.getAttribute("rotation").y - 10; 
+            camera.setAttribute('rotation', {x: prevX, y: newY, z: prevZ });
+        }
 
 
         // head.setAttribute('look-controls', {enabled: false})
-        camera.setAttribute('rotation', {x: prevX, y: newY, z: prevZ });
         // head.setAttribute('look-controls', {enabled: true})
         // if (evt.detail.x < -0.95) { camera.setAttribute('rotation', '0 -90 0'); }
         // if (evt.detail.x > 0.95) { camera.setAttribute('rotation', '0 90 0'); }
